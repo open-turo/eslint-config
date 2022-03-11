c0dac84033d2c5c0616a364894f8f3a33f1444a6
Victor <victor@turo.com>
2022-03-10 19:10:16 -0800

    feat: create initial rules

---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: [
+    "eslint-config-airbnb-base",
+    "eslint:recommended",
+    "prettier",
+    "plugin:sonarjs/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:jest/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:node/recommended",
+  ],
+  plugins: [
+    "import",
+    "json",
+    "import",
+    "jest",
+    "prettier",
+    "simple-import-sort",
+    "sonarjs",
+    "sort-destructure-keys",
+    "unicorn",
+    "node",
+  ],
+  rules: {
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      ecmaFeatures: {
+        modules: true,
+      },
+    },
+    rules: {
+      "import/default": "off",
+      "import/named": "off",
+      "import/namespace": "off",
+      "import/no-default-export": "error",
+      "import/prefer-default-export": "off",
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "sort-destructure-keys/sort-destructure-keys": "error",
+    },
+  },
+};
